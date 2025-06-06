62cf66d2665384277c65b135a6a9c3349171aab5
eskerda <eskerda@gmail.com>
2025-06-06 11:45:42 +0200

    fix zoom/center not persisting

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,13 +1,24 @@
 import { writable } from 'svelte/store'
 
+import { LngLat } from 'maplibre-gl'
+
 export const selectedStation = writable(null)
 export const loading = writable(false)
 
-const last_zoom = localStorage.getItem('last_zoom') ?? undefined
-const last_center = localStorage.getItem('last_center') ?? undefined
+let last_zoom
+let last_center
 
-export const zoom = writable(null)
-export const center = writable(null)
+try {
+  last_zoom = parseInt(localStorage.getItem('last_zoom'))
+  last_center = LngLat.convert(JSON.parse(localStorage.getItem('last_center')))
+} catch (error) {
+  console.log("err", error)
+  last_zoom = null
+  last_center = null
+}
+
+export const zoom = writable(last_zoom)
+export const center = writable(last_center)
 
 // Persist zoom and center to local storage
 zoom.subscribe( async (z) => {
