48b965504e2bff8520182f380b8d871f747c36db
eskerda <eskerda@gmail.com>
2025-06-12 00:42:11 +0200

    track pointerevents instead of touch/mouse
    touch sometimes generates a mouse event. Using pointer
    events means it only happens once. It also means we need
    to track multitouch, but it's not so difficult it seems

--- src/lib/map.js
+++ src/lib/map.js
@@ -3,6 +3,8 @@ const THRESHOLD = 400
 function MapInteractor(map) {
   this.map = map
 
+  this.events = []
+
   this.pointerUp = null
   this.pointerDown = null
   this.pointerEv = null
@@ -13,10 +15,14 @@ function MapInteractor(map) {
 }
 
 MapInteractor.prototype.init = function (onSingleTouch) {
-  this.map.on('touchstart', (e) => {
-    console.log('touchstart', e)
-    // pinch zoom
-    if (e.originalEvent.touches.length > 1) return
+  this.map._container.addEventListener('pointerdown', (e) => {
+    e.point = {
+      x: parseInt(e.x),
+      y: parseInt(e.y),
+    }
+    this.events.push(e)
+    // disable for multi touch
+    if (this.events.length > 1) return
 
     const now = performance.now()
     // < THRESHOLD since last touch, possibly a double tap
@@ -39,43 +45,14 @@ MapInteractor.prototype.init = function (onSingleTouch) {
     this.pointerDown = now
   })
 
-  this.map.on('touchend', (e) => {
-    console.log('touchend', e)
+  this.map._container.addEventListener('pointerup', (e) => {
+    e.point = {
+      x: parseInt(e.x),
+      y: parseInt(e.y),
+    }
+    this.events.pop()
     this.pointerUp = performance.now()
     this.pointerEv = e
-  })
-
-  this.map.on('mousedown', (e) => {
-    // ignore touch events (yes, maplibre does this)
-    if (e.originalEvent.sourceCapabilities.firesTouchEvents) return
-
-    console.log('mousedown', e)
-    const now = performance.now()
-    // possible double click
-    if (now - this.clickUp < THRESHOLD) {
-
-    } else {
-      setTimeout(() => {
-        const now = performance.now()
-        // double click
-        if (now - this.clickDown < THRESHOLD) return
-        // long click ?
-        if (now - this.clickUp > THRESHOLD) return
-        // compare x,y of up and down to see if it was a drag event
-        const { x, y } = this.clickEv.point
-        if (x != e.point.x || y != e.point.y) return
-        onSingleTouch(e)
-      }, THRESHOLD)
-    }
-    this.clickDown = now
-  })
-
-  this.map.on('mouseup', (e) => {
-    // ignore touch events (yes, maplibre does this)
-    if (e.originalEvent.sourceCapabilities.firesTouchEvents) return
-    console.log('mouseup', e)
-    this.clickUp = performance.now()
-    this.clickEv = e
   })
 }
 
